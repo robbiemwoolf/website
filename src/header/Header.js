3bc99cc793a1358c7b8f107dc8fffae289cddf43
Robbie Woolf <robbiemwoolf@gmail.com>
2022-07-16 18:28:26 -0700

    initial upload

---
+++ src/header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className="text-uppercase text-right">Robbie Woolf</h1>
+        </div>
+    )
+}
