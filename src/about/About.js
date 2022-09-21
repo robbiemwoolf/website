77502df73f5bad979e805d11c5d859f54ff4114f
Robbie Woolf <robbiemwoolf@gmail.com>
2022-09-21 08:33:14 -0700

    fixed about me content errors

--- src/about/About.js
+++ src/about/About.js
@@ -5,7 +5,7 @@ export default function About() {
                 <div className='col-md-8'>
                     <div className='card-body'>
                         <h5 className='card-title special'>About Me</h5>
-                        <p className='card-text'>Welder turned software engineer. From building trains to full-stack applications, I love to create. I am proficient in using JavaScript for front-end and back-end programming. Technologies I like to use, include: React, HTML, CSS, API implementing, Node.JS, Express, PostgreSQL, Git, and frameworks like Bootstrap. My experience as a welder provides advantages in detail-oriented builds and keeping client satisfaction as a top priority. Additionally, my familiarity with large scale builds and tight deadlines developed my effectiveness in team collaboration.</p>
+                        <p className='card-text'>Welder turned software engineer; from building trains to full-stack applications, I love to create. I am proficient in using JavaScript for front-end and back-end programming, as well as React, HTML, CSS, API implementing, Node.JS, Express, PostgreSQL, Git, and frameworks like Bootstrap. My experience as a welder provides advantages in detail-oriented builds and keeping client satisfaction as a top priority. Additionally, my familiarity with large scale builds and tight deadlines has improved my effectiveness in team collaboration.</p>
                     </div>
                 </div>
                 <div className='col-md-4'>
