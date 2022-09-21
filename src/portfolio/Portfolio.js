7975119e52bd9afe703a194ddde9158350aaa93d
Robbie Woolf <robbiemwoolf@gmail.com>
2022-09-21 09:48:36 -0700

    created project card component to be reused

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,65 +1,63 @@
+import ProjectCard from "./ProjectCard"
+
 export default function Portfolio() {
+    const projects = [
+        { 
+            title: 'Restaurant Reservation System',
+            site: 'https://reservation-client-rw.herokuapp.com/dashboard',
+            github: 'https://github.com/robbiemwoolf/restaurant-reservation',
+            img: 'images/reservation.png',
+            alt: 'GrubDash Home',
+            description: 'Full-stack application for restaurant personnel to create, update, view, and delete reservations, as well as seat them at various tables within the restaurant.',
+            tools: 'Node | React | Express | Knex | Postgres | Bootstrap5 | JS | HTML | CSS | RESTful API'
+        },
+        {
+            title: 'We Love Movies',
+            site: 'https://we-love-movies-backend-rw.herokuapp.com/movies',
+            github: 'https://github.com/robbiemwoolf/we-love-movies',
+            img: 'images/welovemovies.png',
+            alt: 'We Love Movies Home',
+            description: 'Thinkful project meant to practice building an API following RESTful design principles. Followed a inside-out development workflow.',
+            tools: 'JS | Express | Knex | Jest | React'
+        }
+    ]
+
+    const projects2 = [
+        {
+            title: 'Taskmaster',
+            site: 'https://taskmaster-client-rw.herokuapp.com/',
+            gitHub: 'https://github.com/robbiemwoolf/taskmaster',
+            img: 'images/taskmaster.png',
+            alt: 'Taskmaster Home',
+            description: `This is a personal task tracker app with the key feature being the ability to see what tasks you completed on a given day, week, or month. Many people tend to focus on what they didn't do, but this app celebrates how much you do accomplish.`,
+            tools: 'JS | React | Express | Knex | Bootstrap | CSS'
+        },
+        {
+            title: 'Study Tool',
+            site: 'https://flashcard-project-ten.vercel.app/',
+            gitHub: 'https://github.com/robbiemwoolf/flashcard-project',
+            img: 'images/flashcardomatic.png',
+            alt: 'Flashcard App Alert Home',
+            description: 'Thinkful project meant to test React and API knowledge. This application allows users to create, edit, and delete decks and cards within them.',
+            tools: 'JS | React | Express | Knex | Bootstrap'
+        }
+    ]
+    
+    const info = projects.map((project) => {
+        return <ProjectCard project={project} />
+    })
+
+    const info2 = projects2.map((project) => {
+        return <ProjectCard project={project} />
+    })
+
     return (
         <>
         <div className='card-group m-2'>
-        <div className='card mx-2'>
-            <div className='px-3 pt-3'>
-                <h4 className='card-title special'>Restaurant Reservation System</h4>
-                <a className='link-light' href='https://reservation-client-rw.herokuapp.com/dashboard' title='Live Site'><i className='bi bi-eye'></i></a>
-                <a className='link-light' href='https://github.com/robbiemwoolf/restaurant-reservation' title='GitHub Repository'><i class='bi bi-github mx-2'></i></a>
-            </div>
-            <div className='m-2'>
-                <img className='card-img-top rounded-4' src='images/reservation.png' alt='GrubDash Home' />
-            </div>
-            <div className='card-body'>
-                <p className='card-text'>Full-stack application for restaurant personnel to create, update, view, and delete reservations, as well as seat them at various tables within the restaurant.</p>
-                <p className='card-text'><small className='special'>Node | React | Express | Knex | Postgres | Bootstrap5 | JS | HTML | CSS | RESTful API</small></p>
-            </div>
+        {info}
         </div>
-        <div className='card mx-2'>
-            <div className='px-3 pt-3'>
-                <h4 className='card-title special'>We Love Movies</h4>
-                <a className='link-light' href='https://we-love-movies-backend-rw.herokuapp.com/movies' title='Live Site'><i className='bi bi-eye'></i></a>
-                <a className='link-light' href='https://github.com/robbiemwoolf/we-love-movies' title='GitHub Repository'><i class='bi bi-github mx-2'></i></a>
-            </div>
-          <div className='m-2'>
-            <img className='card-img-top rounded-4' src='images/welovemovies.png' alt='We Love Movies Home' />
-          </div>
-          <div className='card-body'>
-            <p className='card-text'>Thinkful project meant to practice building an API following RESTful design principles. Followed a inside-out development workflow.</p>
-            <p className='card-text'><small className='special'>JS | Express | Knex | Jest | React </small></p>
-          </div>
-        </div>
-      </div>
-      <div className='card-group m-2 pb-4'>
-        <div className='card mx-2'>
-            <div className='px-3 pt-3'>
-                <h4 className='card-title special'>Taskmaster</h4>
-                <a className='link-light' href='https://taskmaster-client-rw.herokuapp.com/' title='Live Site'><i className='bi bi-eye'></i></a>
-                <a className='link-light' href='https://github.com/robbiemwoolf/taskmaster' title='GitHub Repository'><i class='bi bi-github mx-2'></i></a>
-            </div>
-            <div className='m-2'>
-                <img className='card-img-top rounded-4' src='images/taskmaster.png' alt='Taskmaster Home' />
-            </div>
-            <div className='card-body'>
-                <p className='card-text'>This is a personal task tracker app with the key feature being the ability to see what tasks you completed on a given day, week, or month. Many people tend to focus on what they didn't do, but this app celebrates how much you do accomplish.</p>
-                <p className='card-text'><small className='special'>JS | React | Express | Knex | Bootstrap | CSS</small></p>
-            </div>
-        </div>
-        <div className='card mx-2'>
-            <div className='px-3 pt-3'>
-            <h4 className='card-title special'>Study Tool</h4>
-                <a className='link-light' href='https://flashcard-project-ten.vercel.app/' title='Live Site'><i className='bi bi-eye'></i></a>
-                <a className='link-light' href='https://github.com/robbiemwoolf/flashcard-project' title='GitHub Repository'><i class='bi bi-github mx-2'></i></a>
-            </div>
-            <div className='m-2'>
-                <img className='card-img-top rounded-4' src='images/flashcardomatic.png' alt='Flashcard App Alert Home' />
-            </div>
-            <div className='card-body'>
-                <p className='card-text'>Thinkful project meant to test React and API knowledge. This application allows users to create, edit, and delete decks and cards within them.</p>
-                <p className='card-text'><small className='special'>JS | React | Express | Knex | Bootstrap</small></p>
-            </div>
-        </div>
+        <div className='card-group m-2 pb-4'>
+        {info2}
         </div>
         </>
     ) 
---
+++ src/portfolio/ProjectCard.js
@@ -0,0 +1,20 @@
+export default function ProjectCard({project}) {
+    return (
+        <>
+         <div className='card mx-2'>
+            <div className='px-3 pt-3'>
+                <h4 className='card-title special'>{project.title}</h4>
+                <a className='link-light' href={project.site} title='Live Site'><i className='bi bi-eye'></i></a>
+                <a className='link-light' href={project.github} title='GitHub Repository'><i class='bi bi-github mx-2'></i></a>
+            </div>
+            <div className='m-2'>
+                <img className='card-img-top rounded-4' src={project.img} alt={project.alt} />
+            </div>
+            <div className='card-body'>
+                <p className='card-text'>{project.description}</p>
+                <p className='card-text'><small className='special'>{project.tools}</small></p>
+            </div>
+        </div>
+        </>
+    )
+}
