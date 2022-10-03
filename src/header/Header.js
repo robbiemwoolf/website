fe86f49f4dbd7267c5816e02a9c2b8cabfab0996
Robbie Woolf <robbiemwoolf@gmail.com>
2022-10-02 20:53:03 -0700

    changed tagline size h5=>h3

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
             <div className='text-center text-white pt-4'>
                 <h1 className='name text-primary'>Robbie Woolf</h1>
-                <h5 className='tagline'>from welding trains to developing full stack applications</h5>
+                <h3 className='tagline'>from welding trains to developing full stack applications</h3>
             </div>
     )
 }
