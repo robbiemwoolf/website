f1cef42818903f5f5b4fcbd22c00ff553cf9bdde
Robbie Woolf <robbiemwoolf@gmail.com>
2022-10-02 20:06:20 -0700

    updated fonts; get second opinion;

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,8 +3,8 @@ import './Header.css'
 export default function Header() {    
     return (
             <div className='text-center text-white py-4'>
-                <h1 className=''>Robbie Woolf</h1>
-                <p className=''>from welding trains to developing full stack applications</p>
+                <h1 className='name'>Robbie Woolf</h1>
+                <h5 className='tagline'>from welding trains to developing full stack applications</h5>
             </div>
     )
 }
