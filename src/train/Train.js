ebfc1628b885d3221291cfa1f2cd73aebdc57136
Robbie Woolf <robbiemwoolf@gmail.com>
2022-10-03 06:41:19 -0700

    train image fixed; next add logos;

--- src/train/Train.js
+++ src/train/Train.js
@@ -2,7 +2,14 @@ import './Train.css'
 
 export default function Train() {
     return (
-        <div className='hr my-2'>
+        <div className='d-flex hr my-2 justify-content-end'>
+            <img src='./images/car.png' alt='car'></img>
+            <img src='./images/car.png' alt='car'></img>
+            <img src='./images/car.png' alt='car'></img>
+            <img src='./images/car.png' alt='car'></img>
+            <img src='./images/car.png' alt='car'></img>
+            <img src='./images/car.png' alt='car'></img>
+            <img src='./images/train-hr.png' alt='train'></img>
         </div>
     )
 }
