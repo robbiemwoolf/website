e7f42f5f26829d324e9aa05d12e6b103490bac3c
Robbie Woolf <robbiemwoolf@gmail.com>
2022-10-11 17:17:10 -0700

    changed ordering of about elements

--- src/about/About.js
+++ src/about/About.js
@@ -2,6 +2,9 @@ export default function About() {
     return (
         <div className='card mb-3'>
             <div className='d-flex row g-0 align-items-center'>
+            <div className='col-md-4'>
+                    <img src='images/rob_bird_feet_look_left.jpg' className='img-fluid rounded-start gray' alt='Robbie Woolf' />
+                </div>
                 <div className='col-md-8'>
                     <div className='card-body'>
                         <h5 className='card-title special'>About Me</h5>
@@ -9,9 +12,6 @@ export default function About() {
                         <p className='card-text'>My transition into software engineering was almost seamless. My spouse was enrolled in a beginner web development course, and after a week or two of trying my hand at her coursework I was struck with the realization that “I could do this”. Only a short time later and now I’m suddenly building full-stack applications; consuming equal amounts coffee and JavaScript, I am continually increasing my profiency with React, HTML, CSS, API implementation, Node.JS, Express, PostgreSQL, Git, and frameworks like Bootstrap. Outside of my technical competencies, my time as a welder gave me experience with detail-oriented builds, keeping client satisfaction a top priority, and managing large-scale builds and tight deadlines, all of which improved my effectiveness in team collaboration to give me a head start in this new career path of mine.</p>
                         <p className='card-text'>At the end of the day, though, I’m neither a welder nor a software engineer. I’m just Robbie Woolf—hobbies-enthusiast, dog parent of three, environmentalist, horror-genre fanatic, and spicy food devotee.</p>
                     </div>
-                </div>
-                <div className='col-md-4'>
-                    <img src='images/rob_bird_feet_look_left.jpg' className='img-fluid rounded-start' alt='...' />
                 </div>
             </div>
         </div>
