50c0d814299d95fd99c58358b47cfd790b5fa41b
Robbie Woolf <robbiemwoolf@gmail.com>
2022-10-11 18:12:13 -0700

    storing tech-logos in variables

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,6 +1,16 @@
 import ProjectCard from "./ProjectCard"
 
 export default function Portfolio() {
+    const js = <img src='images/tech-logos/JavaScript-logo.png' className='tech-logo neonImg glow' alt='JavaScript' ></img>
+    const react = <img src='images/tech-logos/react-logo.png' className='tech-logo' alt='React' ></img>
+    const node = <img src='images/tech-logos/node-logo.png' className='tech-logo' alt='NodeJS' ></img>
+    const express = <img src='images/tech-logos/express-logo.png' className='tech-logo' alt='Express' ></img>
+    const npm = <img src='images/tech-logos/npm-logo.png' className='tech-logo' alt='npm' ></img>
+    const postgreSQL = <img src='images/tech-logos/PostgreSQL-logo.png' className='tech-logo' alt='PostgreSQL' ></img>
+    const git = <img src='images/tech-logos/git-logo.png' className='tech-logo' alt='Git' ></img>
+    const jQuery = <img src='images/tech-logos/jQuery-logo.png' className='tech-logo' alt='jQuery' ></img>
+    const HTML = <img src='images/tech-logos/html-logo.png' className='tech-logo' alt='HTML' ></img>
+    const CSS = <img src='images/tech-logos/css-logo.jpg' className='tech-logo' alt='CSS' ></img>
     const projects = [
         { 
             title: 'Restaurant Reservation System',
--- src/technologies/Tech.js
+++ src/technologies/Tech.js
@@ -1,20 +1,31 @@
 import './Tech.css'
 
 export default function Train() {
+    const js = <img src='images/tech-logos/JavaScript-logo.png' className='tech-logo neonImg glow' alt='JavaScript' ></img>
+    const react = <img src='images/tech-logos/react-logo.png' className='tech-logo' alt='React' ></img>
+    const node = <img src='images/tech-logos/node-logo.png' className='tech-logo' alt='NodeJS' ></img>
+    const express = <img src='images/tech-logos/express-logo.png' className='tech-logo' alt='Express' ></img>
+    const npm = <img src='images/tech-logos/npm-logo.png' className='tech-logo' alt='npm' ></img>
+    const postgreSQL = <img src='images/tech-logos/PostgreSQL-logo.png' className='tech-logo' alt='PostgreSQL' ></img>
+    const git = <img src='images/tech-logos/git-logo.png' className='tech-logo' alt='Git' ></img>
+    const jQuery = <img src='images/tech-logos/jQuery-logo.png' className='tech-logo' alt='jQuery' ></img>
+    const HTML = <img src='images/tech-logos/html-logo.png' className='tech-logo' alt='HTML' ></img>
+    const CSS = <img src='images/tech-logos/css-logo.jpg' className='tech-logo' alt='CSS' ></img>
+
 
     return (
         <div className='d-flex justify-content-center my-4'>
             <div className='d-flex justify-content-evenly logo-row'>
-                <img src='images/tech-logos/JavaScript-logo.png' className='tech-logo neonImg glow' alt='JavaScript' ></img>
-                <img src='images/tech-logos/react-logo.png' className='tech-logo' alt='React' ></img>
-                <img src='images/tech-logos/node-logo.png' className='tech-logo' alt='NodeJS' ></img>
-                <img src='images/tech-logos/express-logo.png' className='tech-logo' alt='Express' ></img>
-                <img src='images/tech-logos/npm-logo.png' className='tech-logo' alt='npm' ></img>
-                <img src='images/tech-logos/PostgreSQL-logo.png' className='tech-logo' alt='PostgreSQL' ></img>
-                <img src='images/tech-logos/git-logo.png' className='tech-logo' alt='Git' ></img>
-                <img src='images/tech-logos/jQuery-logo.png' className='tech-logo' alt='jQuery' ></img>
-                <img src='images/tech-logos/html-logo.png' className='tech-logo' alt='HTML' ></img>
-                <img src='images/tech-logos/css-logo.jpg' className='tech-logo' alt='CSS' ></img>
+                {js}
+                {react}
+                {node}
+                {express}
+                {npm}
+                {postgreSQL}
+                {git}
+                {jQuery}
+                {HTML}
+                {CSS}
             </div>
         </div>
     )
