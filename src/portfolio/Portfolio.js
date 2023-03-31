26c2511db6b10929190aa41552aef78f2348fbd4
Robbie Woolf <robbiemwoolf@gmail.com>
2023-03-31 16:15:57 -0700

    updated twisty byte

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -43,15 +43,15 @@ export default function Portfolio() {
     ]
 
     const bytes = [
-        { 
-            title: 'Metronome',
-            site: '',
-            github: 'https://github.com/robbiemwoolf/guitar-groove',
-            img: 'images/metronome.png',
-            alt: 'metronome screenshot',
-            description: 'This metronome is the first feature in my guitar application that I will be using to become a better web developer and guitar player. Highlight of this project is finally creating my own custom JavaScript toolchain. No more hours will be wasted sifting through all the deprecations that come with that simple command, `create-react-app`.',
-            tools: ''
-        },
+        // { 
+        //     title: 'Metronome',
+        //     site: '',
+        //     github: 'https://github.com/robbiemwoolf/guitar-groove',
+        //     img: 'images/metronome.png',
+        //     alt: 'metronome screenshot',
+        //     description: 'This metronome is the first feature in my guitar application that I will be using to become a better web developer and guitar player. Highlight of this project is finally creating my own custom JavaScript toolchain. No more hours will be wasted sifting through all the deprecations that come with that simple command, `create-react-app`.',
+        //     tools: ''
+        // },
         {
             title: 'Taskmaster',
             site: 'https://taskmaster-rw.vercel.app',
@@ -61,15 +61,15 @@ export default function Portfolio() {
             description: `This is a personal task tracker app with the key feature being the ability to see what tasks you completed on a given day, week, or month. Many people tend to focus on what they didn't do, but this app celebrates how much you do accomplish.`,
             tools: ''//'JS | React | Express | Knex | Bootstrap | CSS'
         },
-        // {
-        //     title: 'Twisty',
-        //     site: '',
-        //     gitHub: 'https://github.com/robbiemwoolf/twisty', 
-        //     img: 'images/twisty.png',
-        //     alt: 'Twisty screenshot',
-        //     description: `A simple spinner application meant to be used with the classic Twister play mat. I used an infinite animation loop that pauses on hover to determine the color and body part.`,
-        //     tools: ''//'JS | React | CSS'
-        // }
+        {
+            title: 'Twisty',
+            site: 'https://twisty.vercel.app',
+            gitHub: 'https://github.com/robbiemwoolf/twisty', 
+            img: 'images/twisty.png',
+            alt: 'Twisty screenshot',
+            description: `A simple spinner application meant to be used with the classic Twister play mat. I used an infinite animation loop that upon clicking the spinner a random CSS class is chosen and added to the targeted element in order to stop the animation, display a color, and display a body part. After two and half seconds has elapsed the CSS class will be removed and the animation will restart.`,
+            tools: ''//'JS | CSS | React'
+        }
     ]
     
     const projectCards = projects.map((card) => {
