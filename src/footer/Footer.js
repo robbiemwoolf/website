b89ed13c753cd3f2f3a659331cae25dcca994537
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-04 17:23:23 -0700

    links now open a new tab

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,11 +2,35 @@ import './Footer.css'
 
 export default function Footer() {
     
+    const openLinkedIn = () => {
+        const link = 'https://www.linkedin.com/in/robbiewoolf/'
+        const win = window.open(link, '_blank')
+        if (win) {
+            // browser has allowed it to be opened
+            win.focus()
+        } else {
+            // browser has blocked it
+            alert('Please allow popups for this website')
+        }
+    }
+
+    const openGitHub = () => {
+        const link = 'https://github.com/robbiemwoolf'
+        const win = window.open(link, '_blank')
+        if (win) {
+            // browser has allowed it to be opened
+            win.focus()
+        } else {
+            // browser has blocked it
+            alert('Please allow popups for this website')
+        }
+    }
+
     return (
-        <div className='container'>
-            <a href='https://www.linkedin.com/in/robbiewoolf/'>LINKEDIN</a>
+        <div className='footer'>
+            <p className='profile' onClick={openLinkedIn}>LINKEDIN</p>
             <p className='name'>RW</p>
-            <a href='https://github.com/robbiemwoolf'>GITHUB</a>
+            <p className='profile' onClick={openGitHub}>GITHUB</p>
         </div>
     )
 }
--- src/portfolio/ProjectCard.js
+++ src/portfolio/ProjectCard.js
@@ -2,17 +2,40 @@ import './Portfolio.css'
 
 export default function ProjectCard({ card }) {
     const live = card.site;
+    const github = card.github
     const tools = card.tools;
+
+    const openLive = () => {
+        const win = window.open(live, '_blank')
+        if (win) {
+            // browser has allowed it to be opened
+            win.focus()
+        } else {
+            // browser has blocked it
+            alert('Please allow popups for this website')
+        }
+    }
+
+    const openGitHub = () => {
+        const win = window.open(github, '_blank')
+        if (win) {
+            // browser has allowed it to be opened
+            win.focus()
+        } else {
+            // browser has blocked it
+            alert('Please allow popups for this website')
+        }
+    }
 
     return (
         <>
         <div className='card'>
             <div className='head'>
                 { live.length > 0 ? (
-                    <a href={card.site} title='Live Site'><img className='link' src='images/openWindow.png' alt='Open Live Site' /></a>
+                <img className='link' src='images/openWindow.png' alt='Open Live Site' onClick={openLive} />
                 ) : null }
                 <h4 className='title'>{card.title}</h4>
-                <a href={card.github} title='GitHub Repository'><img className='link' src='images/gitHub.png' alt='Open GitHub Repo' /></a>
+                <img className='link' src='images/gitHub.png' alt='Open GitHub Repo' onClick={openGitHub} />
             </div>
             <div className='filter'>
                 <img className='screenshot' src={card.img} alt={card.alt} />
