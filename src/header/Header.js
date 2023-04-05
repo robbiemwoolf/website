f474ac98bca59631c901770e82343111d39f24ff
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-04 19:37:29 -0700

    hamburger menu ready for event listener

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,21 @@
 import './Header.css'
 
-export default function Header() {    
+export default function Header() {  
+
+
     return (
             <div className='header'>
-                <h1>Woolf</h1>
+                <div className='container'><h1>Woolf</h1></div>
+                {/* <nav>
+                    <a href='#'>Home</a>
+                    <a href='#'>About</a>
+                    <a href='#'>Projects</a>
+                    <a href='#'>Contact</a>
+                    <a href='#'>Resume</a>
+                </nav> */}
+                <button className='hamburger is-active'>
+                    <div className='bar'></div>
+                </button>
             </div>
     )
 }
