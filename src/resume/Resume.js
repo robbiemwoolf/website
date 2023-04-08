8ef672d722ceb452255f23b885a9400a25f7ba48
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-08 11:03:44 -0700

    buttons created - still NOT functional

--- src/resume/Resume.js
+++ src/resume/Resume.js
@@ -2,9 +2,13 @@ import './Resume.css'
 
 export default function Resume() {
     return (
-            <>
+        <>
+            <div className='resume-links'>
+                <button className='btn'>Download PDF</button>
+                <button className='btn'>Download DOCX</button>
+            </div>
             <div className='embed'>
-                <iframe title='resume' src="https://drive.google.com/file/d/12nOdvGSl3ouV4xKgX1oW4wi16qBCkHhc/preview" width="90%" height="480" allow="autoplay"></iframe>
+                <iframe title='resume' src="https://drive.google.com/file/d/12nOdvGSl3ouV4xKgX1oW4wi16qBCkHhc/preview" width="640" height="480" allow="autoplay"></iframe>
             </div>
         </>
     )
