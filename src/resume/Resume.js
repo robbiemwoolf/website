59916e314a81ffbf5a7823fcc8264478a39981bf
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-08 10:57:29 -0700

    resume embedded;

--- src/resume/Resume.js
+++ src/resume/Resume.js
@@ -1,5 +1,11 @@
+import './Resume.css'
+
 export default function Resume() {
     return (
-        <p name='resume' id='resume'>resume page</p>
+            <>
+            <div className='embed'>
+                <iframe title='resume' src="https://drive.google.com/file/d/12nOdvGSl3ouV4xKgX1oW4wi16qBCkHhc/preview" width="90%" height="480" allow="autoplay"></iframe>
+            </div>
+        </>
     )
 }
