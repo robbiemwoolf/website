4024f94184d88d3ad53ab1e0143a4aae12cd25eb
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-08 07:46:45 -0700

    sticky header complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './header/Header'
 import Tech from './technologies/Tech'
@@ -6,13 +7,25 @@ import Portfolio from './portfolio/Portfolio'
 import Footer from './footer/Footer'
 
 function App() {
+    const [offset, setOffset] = useState(0)
+
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset)
+
+        window.removeEventListener('scroll', onScroll)
+        window.addEventListener('scroll', onScroll, { passive: true })
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [])
+
   return (
     <div className="App">
       <Header />
-      <Tech />
-      <About />
-      <Portfolio />
-      <Footer />
+      <div className={(offset > 0) ? 'content' : 'null'}>
+        <Tech />
+        <About />
+        <Portfolio />
+        <Footer />
+      </div>
     </div>
   )
 }
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,20 @@
 import './Header.css'
 import Menu from './Menu'
+import React, { useEffect, useState } from 'react'
 
-export default function Header() {  
+export default function Header() {
+    const [offset, setOffset] = useState(0)
+
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset)
+
+        window.removeEventListener('scroll', onScroll)
+        window.addEventListener('scroll', onScroll, { passive: true })
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [])
+
     return (
-            <div className='header'>
+            <div className={(offset > 0) ? "header sticky" : "header"}>
                 <div className='container'><h1>Woolf</h1></div>
                 <Menu />               
             </div>
