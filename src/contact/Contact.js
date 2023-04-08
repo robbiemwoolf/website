affdc950d8b8acaf7dcdbefbf69ff14836fb8918
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-08 16:49:09 -0700

    contact form sends email and displays confirmation

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,75 +1,105 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import emailjs from '@emailjs/browser';
 import './Contact.css'
 
 export default function Contact() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [hidden, setHidden] = useState(false)
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+    const [status, setStatus] = useState('')
 
-    const toggle = () => { setHidden(!hidden) }
-    const form = (
-        <form>
-            <div className='form-group'>
-                <label htmlFor="name">
-                    <input 
-                        type="text"
-                        id="name"
-                        placeholder="Your Name"
-                        name="name"
-                        required
-                        value={name}
-                        onChange={(e)=>setName(e.target.value)}
-                    />
-                </label>
-            </div>
-            <div className='form-group'>
-                <label htmlFor="email">
-                    <input 
-                        type="text"
-                        id="email"
-                        placeholder="Your Email"
-                        name="email"
-                        required
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}
-                    />
-                </label>
-            </div>
-            <div className='form-group'>
-                <label htmlFor="message">
-                    <textarea
-                        type="text"
-                        id="message"
-                        placeholder="Your Message"
-                        name="message"
-                        required
-                        value={message}
-                        onChange={(e)=>setMessage(e.target.value)}
-                    />
-                </label>
-            </div>
-        </form>
-    )
+    const form = useRef()
 
-    const confirmation = (
-        <p className='confirmation'>Thanks for reaching out! You can expect a reply back within 1 - 2 business days.</p>
-    )
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_s1f7l2q', 'template_mv461tv', form.current, 'MS2tFnre-NiSuWtl7')
+          .then((response) => {
+              console.log('200 - email sent', response)
+              setValues({
+                name: '',
+                email: '',
+                message: ''
+              })
+              setStatus('SUCCESS')
+          }, (error) => {
+              console.log('FAILED ...', error)
+          })
+    }
 
-    // eslint-disable-next-line no-unused-vars
-    const [_body, setBody] = useState(form)
+    useEffect(() => {
+        if (status === 'SUCCESS') {
+            setTimeout(() => {
+                setStatus('')
+            }, 3000)
+        }
+    }, [status])
 
-    // const emailToMe = `Name: ` + name + `<br /> email: ` + email + `<br /> message: ` + message
+    const handleChange = (e) => {
+        setValues((values) => ({
+            ...values,
+            [e.target.name]: e.target.value
+          }))
+    }
+
+    
+    const confirmation = () => {
+        return (
+            <div className='confirmation'>
+                <p>Thanks for reaching out! You can expect a reply back within 1 - 2 business days.</p>
+            </div> 
+        )  
+    }
 
     return (
         <div className='contact'>
             <header>Contact</header>
             <div className='body'>
-                {hidden ? confirmation : form }
-                <button id='btn' type="submit" className={hidden ? 'hidden' : 'null'} onClick={()=>{
-                    toggle()
-                    setBody(confirmation)
-                }}>Send</button>
+                {status && confirmation()}
+                <form ref={form} onSubmit={handleSubmit}>
+                    <div className='form-group'>
+                        <label htmlFor="name">
+                            <input 
+                                type="text"
+                                id="name"
+                                placeholder="Your Name"
+                                name="name"
+                                required
+                                value={values.name}
+                                onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="email">
+                            <input 
+                                type="text"
+                                id="email"
+                                placeholder="Your Email"
+                                name="email"
+                                required
+                                value={values.email}
+                                onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="message">
+                            <textarea
+                                type="text"
+                                id="message"
+                                placeholder="Your Message"
+                                name="message"
+                                required
+                                value={values.message}
+                                onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <input type="submit" value="send" />
+                </form>
             </div>
         </div>
     )
