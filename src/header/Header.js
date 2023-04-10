4d00bee5c51e1a1504685ce7994cf95d0958e377
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-10 00:43:02 -0700

    desktop navbar created and functional

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,4 +1,5 @@
 import './Header.css'
+import './Nav.css'
 import Menu from './Menu'
 import React, { useEffect, useState } from 'react'
 
@@ -17,6 +18,12 @@ export default function Header() {
             <div className={(offset > 0) ? "header sticky" : "header"}>
                 <a href='#contact' alt='Send me a message!'><img className='ctaHead' alt='contact me' src='images/contactMe.svg'></img></a>
                 <div className='container'><h1>Woolf</h1></div>
+                <div className='navRow'>
+                    <a href='#about' className='linkD'><div>About</div></a>
+                    <a href='#portfolio' className='linkD'><div>Projects</div></a>
+                    <a href='#contact' className='linkD'><div>Contact</div></a>
+                    <a href='/resume' className='linkD'><div>Resume</div></a>
+                </div>
                 <Menu />               
             </div>
     )
